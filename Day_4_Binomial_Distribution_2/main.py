8d0f1b83bbc74b88403421a5a163656533372af7
prabha <Prabhakaran.mails@gmail.com>
2020-09-23 23:35:43 -0400

    added Day 4 second probelm

---
+++ Day_4_Binomial_Distribution_2/main.py
@@ -0,0 +1,16 @@
+import math 
+
+p = 0.88
+q= 1-p
+prob = 0
+
+for i in [8,9,10]:
+    c = math.factorial(10)/(math.factorial(i)*math.factorial(10-i))
+    prob += c*(p**i)*(q**(10-i))
+print(round(prob,3))
+
+prob = 0
+for i in [8,7,6,5,4,3,2,1,0]:
+    c = math.factorial(10)/(math.factorial(i)*math.factorial(10-i))
+    prob += c*(p**i)*(q**(10-i))
+print(round(prob,3))
